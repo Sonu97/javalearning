0fca51a30fd2b6817fe42eccd00b3b96bcc2b9ed
sonu <43224767+Sonu97@users.noreply.github.com>
2022-10-18 09:30:47 -0400

    initial commit

---
+++ src/main/java/javalearning/MathematicalOperators.java
@@ -0,0 +1,39 @@
+package javalearning;
+
+public class MathematicalOperators {
+
+	public static void main(String[] args) {
+		//define variables
+		
+		int firstValue = 15;
+		int secondValue = 2;
+		
+		//addition
+		int a = firstValue + secondValue;
+		System.out.println("- Additon of firstValue and secondValue(15+2): "+a );
+
+		 // subtraction
+		int b = firstValue - secondValue;
+		System.out.println("- Subtraction of firstValue and secondValue(15-2): "+b);
+		
+		// multiplication
+		int c = firstValue * secondValue;
+		System.out.println("- Multiplcation of firstValue and secondValue(15*2): "+c);
+		
+		// division
+		int d = firstValue / secondValue;
+		System.out.println("- Division of firstValue and secondValue(15/2): "+d);
+		
+		// modulo
+		int e = firstValue + secondValue;
+		System.out.println("- Modulo of firstValue and secondValue(15%2): "+e);
+		
+
+				
+	}
+	
+
+
+	}
+
+
